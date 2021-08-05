86a14ff25b07da3b2111632fc4ba89064c8ae79c
Gustavo Antonio Lutz de Matos <gustavo.almatos@gmail.com>
2021-08-05 19:10:02 -0300

    fix: ajustes de segurança

--- src/main/java/br/com/lutztechnology/appveterinario/config/WebSecurityConfigurerAdapterImpl.java
+++ src/main/java/br/com/lutztechnology/appveterinario/config/WebSecurityConfigurerAdapterImpl.java
@@ -1,16 +1,13 @@
 package br.com.lutztechnology.appveterinario.config;
 
-import br.com.lutztechnology.appveterinario.domain.service.AppUserDetailsServiceImpl;
+import br.com.lutztechnology.appveterinario.services.AppUserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
