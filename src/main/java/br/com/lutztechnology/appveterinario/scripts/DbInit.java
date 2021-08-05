3fd6fead00672d6de002da52644d838835992c68
Gustavo Antonio Lutz de Matos <gustavo.almatos@gmail.com>
2021-08-05 19:11:42 -0300

    fix: add condicional para execução do script

--- src/main/java/br/com/lutztechnology/appveterinario/scripts/DbInit.java
+++ src/main/java/br/com/lutztechnology/appveterinario/scripts/DbInit.java
@@ -27,34 +27,37 @@ public class DbInit implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        Role role = new Role("Gerente");
-        roleRepository.save(role);
 
-        Employee admin = new Employee(
-                "glutzm@gmail.com",
-                "Gustavo Matos",
-                "046.343.021-28",
-                "(11) 97393-6293",
-                LocalDate.parse("1992-08-08"),
-                Address.builder()
-                        .state(State.SP)
-                        .city("São Paulo")
-                        .district("Jardim Ubirajara")
-                        .publicPlace("Rua Zike Tuma")
-                        .number("224")
-                        .cep("04458-000")
-                        .complement("Casa 44")
-                        .build(),
-                passwordEncoder.encode("12345"),
-                LocalDate.parse("2020-10-29"),
-                null,
-                true,
-                "Cardiologista",
-                State.MS,
-                "123456",
-                role,
-                null);
+        if (roleRepository.findAll().isEmpty()) {
+            Role role = new Role("Gerente");
+            roleRepository.save(role);
 
-        employeeRepository.save(admin);
+            Employee admin = new Employee(
+                    "glutzm@gmail.com",
+                    "Gustavo Matos",
+                    "046.343.021-28",
+                    "(11) 97393-6293",
+                    LocalDate.parse("1992-08-08"),
+                    Address.builder()
+                            .state(State.SP)
+                            .city("São Paulo")
+                            .district("Jardim Ubirajara")
+                            .publicPlace("Rua Zike Tuma")
+                            .number("224")
+                            .cep("04458-000")
+                            .complement("Casa 44")
+                            .build(),
+                    passwordEncoder.encode("12345"),
+                    LocalDate.parse("2020-10-29"),
+                    null,
+                    true,
+                    "Cardiologista",
+                    State.MS,
+                    "123456",
+                    role,
+                    null);
+
+            employeeRepository.save(admin);
+        }
     }
 }
