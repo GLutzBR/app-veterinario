cee566820c7cc6f2880bf9feb3f506a024e2e1d0
Gustavo Antonio Lutz de Matos <gustavo.almatos@gmail.com>
2021-08-18 23:17:44 -0300

    refactor: pequenos ajustes na aplicação

--- src/main/java/br/com/lutztechnology/appveterinario/repository/AddressRepository.java
+++ src/main/java/br/com/lutztechnology/appveterinario/repository/AddressRepository.java
@@ -2,6 +2,8 @@ package br.com.lutztechnology.appveterinario.repository;
 
 import br.com.lutztechnology.appveterinario.model.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AddressRepository extends JpaRepository<Address, Long> {
 }
