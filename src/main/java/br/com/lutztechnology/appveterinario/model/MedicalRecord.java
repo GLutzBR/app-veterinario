55d1bc1edaa493aa40dc70f4f474ae1533995171
Gustavo Antonio Lutz de Matos <gustavo.almatos@gmail.com>
2021-11-27 18:40:38 -0300

    fix: corrigido busca de entidades

--- src/main/java/br/com/lutztechnology/appveterinario/model/MedicalRecord.java
+++ src/main/java/br/com/lutztechnology/appveterinario/model/MedicalRecord.java
@@ -1,8 +1,7 @@
 package br.com.lutztechnology.appveterinario.model;
 
-//import br.com.lutztechnology.appveterinario.serialize.MedicalRecordSerializer;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-//import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -11,8 +10,6 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PastOrPresent;
 import java.time.LocalDate;
 
-// TODO: reavaliar serialização
-//@JsonSerialize(using = MedicalRecordSerializer.class)
 @Entity
 @Table(name = "medical_records")
 @NoArgsConstructor
@@ -21,12 +18,14 @@ import java.time.LocalDate;
 @Getter @Setter
 public class MedicalRecord extends BaseEntity {
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
     @NotNull
     @PastOrPresent
     @Column(nullable = false, name = "service_date")
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private LocalDate serviceDate;
 
+    @JsonIgnore
     @NotNull
     @Column(nullable = false, columnDefinition = "TEXT")
     private String comments;
@@ -35,21 +34,18 @@ public class MedicalRecord extends BaseEntity {
     @Column(nullable = false)
     private Boolean archived = false;
 
-    @JsonIgnore
     @NotNull
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "customer_id_fk", nullable = false)
     private Customer customer;
 
-    @JsonIgnore
     @NotNull
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "animal_id_fk", nullable = false)
     private Animal animal;
 
-    @JsonIgnore
     @NotNull
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "employee_id_fk", nullable = false)
     private Employee veterinarian;
 }
